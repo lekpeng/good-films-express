62711c52e2e52d43a0355048e33ea6e0d8e823ea
MarcusMo <marcus400.1@gmail.com>
2022-09-07 23:08:32 +0800

    added login and register. to continue w validation and jwt

---
+++ controllers/user_controller.js
@@ -0,0 +1,72 @@
+const bcrypt = require("bcrypt");
+const userModel = require("../models/user");
+const jwt = require("jsonwebtoken");
+
+module.exports = {
+  register: async (req, res) => {
+    // do validations ...
+
+    const validatedValues = req.body;
+    console.log("req.body: ", req.body);
+
+    // might need to add validation for username? which one are we using as unique identifier?
+    try {
+      const user = await userModel.findOne({ email: validatedValues.email });
+      if (user) {
+        return res.status(409).json({ error: "user exists" });
+      }
+    } catch (err) {
+      return res.status(500).json({ error: "failed to get user" });
+    }
+
+    const passHash = await bcrypt.hash(req.body.password, 10);
+    const user = { ...req.body, password: passHash };
+
+    try {
+      await userModel.create(user);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json({ error: "failed to register user" });
+    }
+
+    return res.status(200).json("User successfully created");
+  },
+
+  login: async (req, res) => {
+    // do validations ...
+
+    const validatedValues = req.body;
+    let errMsg = "username or password is incorrect";
+    let user = null;
+
+    try {
+      user = await userModel.findOne({ username: validatedValues.username });
+      if (!user) {
+        return res.status(401).json({ error: errMsg });
+      }
+    } catch (err) {
+      return res.status(500).json({ error: "failed to get user" });
+    }
+
+    const isPasswordOk = await bcrypt.compare(req.body.password, user.password);
+
+    if (!isPasswordOk) {
+      return res.status(401).json({ error: errMsg });
+    }
+
+    // generate JWT and return as response
+    const userData = {
+      email: user.email,
+      name: user.name,
+    };
+    const token = jwt.sign(
+      {
+        exp: Math.floor(Date.now() / 1000) + 60 * 60,
+        data: userData,
+      },
+      process.env.JWT_SECRET
+    );
+
+    return res.json({ token });
+  },
+};
