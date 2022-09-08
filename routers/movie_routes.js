0a0aa9d80c79c180f8d3665f5753db108deaf991
wenxi1608 <wenxihe1@gmail.com>
2022-09-09 00:10:05 +0800

    feat: added popular and top rated movie API routes

--- routers/movie_routes.js
+++ routers/movie_routes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 // GET /
 router.get("/:movieApiId", movieController.showMovie);
-router.get("/", movieController.showTrending);
+router.get("/popular", movieController.showPopular);
+router.get("/top_rated", movieController.showTopRated);
 
 module.exports = router;
