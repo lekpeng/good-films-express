fe0e9a693ed0ace3ba2f5c65ea4ce921fc2b3539
wenxi1608 <wenxihe1@gmail.com>
2022-09-12 21:19:48 +0800

    feat: updated POST route for movie rating

--- routers/movie_routes.js
+++ routers/movie_routes.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const movieController = require("../controllers/movie_controller");
+const reviewController = require("../controllers/review_controller");
 const router = express.Router();
+const authMiddleWare = require("../middleware/authmiddleware");
 
-// GET /
+// GET MOVIE DETAILS
 router.get("/:movieApiId", movieController.showMovie);
 router.get("/popular", movieController.showPopular);
 router.get("/top_rated", movieController.showTopRated);
+router.get("/:genre/:genreId", movieController.filterByGenre);
+
+// RATE MOVIE
+router.post("/:movieApiId", authMiddleWare, reviewController.submitRating);
 
 module.exports = router;
