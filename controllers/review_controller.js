c732bb2868a73625a479986432796dbd9ef99abe
wenxi1608 <wenxihe1@gmail.com>
2022-09-14 23:17:37 +0800

    fix: updated routes for user to submit review

--- controllers/review_controller.js
+++ controllers/review_controller.js
@@ -2,41 +2,67 @@ const axios = require("axios");
 const cors = require("cors");
 const Review = require("../models/review");
 const User = require("../models/user");
+const Movie = require("../models/movie");
 
 module.exports = {
   submitRating: async (req, res) => {
     const currentUserAuthDetails = res.locals.userAuth;
     const currentUserUsername = currentUserAuthDetails.data.username;
-    const movieApiId = req.params.movieApiId;
+    const movieReviewed = req.params.movieApiId;
     const userRating = req.body.rating;
+    const userReview = req.body.review.text;
 
-    try {
-      // find user
-      const user = await User.findOneAndUpdate(
-        { username: currentUserUsername },
-        {
-          $push: {
-            reviewIds: {
-              movieId: movieApiId,
-              rating: userRating,
-            },
+    const movieExists = await Movie.findOne({ movieApiId: movieReviewed });
+    console.log("Movie:", movieExists);
+
+    // check if movieId exists in DB
+
+    // if doesn't exist, create a new movie doc
+
+    if (!movieExists) {
+      try {
+        await Movie.create({
+          movieApiId: movieReviewed,
+          reviewIds: {
+            movieId: movieReviewed,
+            reviewText: userReview,
+            rating: userRating,
           },
-        }
-      );
-
-      if (!user) {
-        res.status(404);
-        return res.json({ error: `User ${user} does not exist!` });
+        });
+      } catch (error) {
+        console.log(error);
+        return res.status(400).json({ error: "Unable to create movie review" });
       }
-    } catch (error) {
-      console.log(error);
-      return res.status(400).json({ error: "Failed to rate movie" });
     }
-
-    console.log("Rating:", userRating.rating);
-    console.log("User:", user);
-    console.log("Current User Name:", currentUserUsername);
   },
+
+  //   try {
+  //     // find user
+  //     const user = await User.findOneAndUpdate(
+  //       { username: currentUserUsername },
+  //       {
+  //         $push: {
+  //           reviewIds: {
+  //             movieId: movieApiId,
+  //             rating: userRating,
+  //           },
+  //         },
+  //       }
+  //     );
+
+  //     if (!user) {
+  //       res.status(404);
+  //       return res.json({ error: `User ${user} does not exist!` });
+  //     }
+  //   } catch (error) {
+  //     console.log(error);
+  //     return res.status(400).json({ error: "Failed to rate movie" });
+  //   }
+
+  //   console.log("Rating:", userRating.rating);
+  //   console.log("User:", user);
+  //   console.log("Current User Name:", currentUserUsername);
+  // },
 
   updateLikes: async (req, res) => {
     const reviewId = req.body.reviewId;
--- routers/review_routes.js
+++ routers/review_routes.js
@@ -3,8 +3,7 @@ const reviewController = require("../controllers/review_controller");
 const authMiddleWare = require("../middleware/authmiddleware");
 const router = express.Router();
 
-router.post("/rating", reviewController.submitRating);
-
+router.post("/:movieApiId", authMiddleWare, reviewController.submitRating);
 router.patch("/like", authMiddleWare, reviewController.updateLikes);
 router.patch("/unlike", authMiddleWare, reviewController.updateLikes);
 
